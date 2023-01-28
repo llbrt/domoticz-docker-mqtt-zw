849ded2cd2b276bf0b6ecdfb62634385b7d2ca22
Loïc Lambert <7870088+llbrt@users.noreply.github.com>
2023-01-28 16:24:20 +0100

    Minor cleaning

--- skel/domoticz/customstart.sh
+++ skel/domoticz/customstart.sh
@@ -2,7 +2,7 @@
 
 USERDATA_DIR=/opt/domoticz/userdata
 echo "Custom start"
-mkdir -p "$USERDATA_DIR"/scripts/dzVents/generated_scripts
 mkdir -p "$USERDATA_DIR"/scripts/dzVents/data
+mkdir -p "$USERDATA_DIR"/scripts/dzVents/generated_scripts
 cp -rp "$USERDATA_DIR"/files/* "$USERDATA_DIR"/../
 echo "Custom start done"
