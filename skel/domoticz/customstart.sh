237f152e8bdbcae464b06b6b412a25b043b7c69d
Loïc Lambert <7870088+llbrt@users.noreply.github.com>
2023-01-28 15:04:35 +0100

    Fix dzVents data persistence
    - Create missing directory in domoticz user data for
      the dzVents persistence

--- skel/domoticz/customstart.sh
+++ skel/domoticz/customstart.sh
@@ -3,5 +3,6 @@
 USERDATA_DIR=/opt/domoticz/userdata
 echo "Custom start"
 mkdir -p "$USERDATA_DIR"/scripts/dzVents/generated_scripts
+mkdir -p "$USERDATA_DIR"/scripts/dzVents/data
 cp -rp "$USERDATA_DIR"/files/* "$USERDATA_DIR"/../
 echo "Custom start done"
