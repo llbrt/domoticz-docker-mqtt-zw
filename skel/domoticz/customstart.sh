ff1c004c11446f3ba159a1814f0cc21a576d39a9
Loïc Lambert <7870088+llbrt@users.noreply.github.com>
2023-02-07 14:48:39 +0100

    Create missing folder for Domoticz

--- skel/domoticz/customstart.sh
+++ skel/domoticz/customstart.sh
@@ -4,5 +4,6 @@ USERDATA_DIR=/opt/domoticz/userdata
 echo "Custom start"
 mkdir -p "$USERDATA_DIR"/scripts/dzVents/data
 mkdir -p "$USERDATA_DIR"/scripts/dzVents/generated_scripts
+mkdir -p "$USERDATA_DIR"/scripts/dzVents/scripts
 cp -rp "$USERDATA_DIR"/files/* "$USERDATA_DIR"/../
 echo "Custom start done"
